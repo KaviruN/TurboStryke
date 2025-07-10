25bf6f4773bd7f64dc6336970afa8c61823b5afc
KaviruN <kavirunethsara1@gmail.com>
2025-07-10 20:41:54 +0530

    refactor: improve code formatting and indentation in Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,37 +1,59 @@
-import { useState } from 'react';
-import logo from '../assets/logo.png';
+import { useState } from "react";
+import logo from "../assets/logo.png";
 
 function Nav() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <nav className="nav">
-            <div className="logo">
-                <img src={logo} alt="TurboStryke Logo" />
-            </div>
+  return (
+    <nav className="nav">
+      <div className="logo">
+        <a href="/">
+          <img src={logo} alt="TurboStryke Logo" />
+        </a>
+      </div>
 
-            {/* Hamburger Menu Button */}
-            <button
-                className="hamburger-menu"
-                onClick={toggleMenu}
-                aria-label="Toggle navigation menu"
-            >
-                <i className="ri-menu-line"></i>
-            </button>
+      {/* Hamburger Menu Button */}
+      <button
+        className="hamburger-menu"
+        onClick={toggleMenu}
+        aria-label="Toggle navigation menu"
+      >
+        <i className="ri-menu-line"></i>
+      </button>
 
-            {/* Navigation Links */}
-            <ul className={`nav-links ${isMenuOpen ? 'nav-links-open' : ''}`}>
-                <li><a href="/" className='nav-active' onClick={() => setIsMenuOpen(false)}>Home</a></li>
-                <li><a href="/tuning-options" onClick={() => setIsMenuOpen(false)}>Tuning Options</a></li>
-                <li><a href="/about" onClick={() => setIsMenuOpen(false)}>About</a></li>
-                <li><a href="/contact-us" onClick={() => setIsMenuOpen(false)}>Contact us</a></li>
-            </ul>
-        </nav>
-    );
+      {/* Navigation Links */}
+      <ul className={`nav-links ${isMenuOpen ? "nav-links-open" : ""}`}>
+        <li>
+          <a
+            href="/"
+            className="nav-active"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/tuning-options" onClick={() => setIsMenuOpen(false)}>
+            Tuning Options
+          </a>
+        </li>
+        <li>
+          <a href="/about" onClick={() => setIsMenuOpen(false)}>
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/contact-us" onClick={() => setIsMenuOpen(false)}>
+            Contact us
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
