368989838b16e28fa326b6061984dade0f5ba284
KaviruN <kavirunethsara1@gmail.com>
2025-07-10 20:29:05 +0530

    feat: integrate Services page and enhance App layout with WavyBackground

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import Nav from "./components/Nav"
 import Home from "./pages/Home"
+import Services from "./pages/Services"
+import { WavyBackground } from "./components/WavyBackground"
 
 function App() {
 
   return (
     <div className="container">
+      <WavyBackground />
       <Nav />
       <Home />
+      <Services />
     </div>
   )
 }
---
+++ src/pages/Services.jsx
@@ -0,0 +1,18 @@
+function Services() {
+  return (
+    <section className="services">
+      <div className="services-header">
+        <h1 className="main-title">
+          <span className="title-line highlight">UNLEASH</span>
+          <span className="title-line">YOUR CAR'S TRUE POTENTIAL</span>
+        </h1>
+        <p className="subtitle">
+          Enhance your vehicle's power and efficiency with our expert tuning
+          services.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Services;
