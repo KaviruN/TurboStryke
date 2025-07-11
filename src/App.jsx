43cd8e34c389b7666a6353fb40f584a65688b8dd
KaviruN <kavirunethsara1@gmail.com>
2025-07-11 15:19:58 +0530

    refactor: add Register component and update routing in App; adjust PageUP position and enhance styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { WavyBackground } from "./components/WavyBackground";
+import PageUP from "./components/PageUp";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
-import PageUP from "./components/PageUp";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
         <PageUP />
       </BrowserRouter>
