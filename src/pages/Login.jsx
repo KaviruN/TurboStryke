594f420e96385b3d337c3667f6f2b6b095fbba40
KaviruN <kavirunethsara1@gmail.com>
2025-07-11 14:59:40 +0530

    refactor: restructure Login component layout and enhance styling for improved user experience

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,28 +2,30 @@ function Login() {
   document.title = "Login";
   return (
     <div className="user wrapper">
-      <div className="user-content">
-        <h1 className="main-title user-title">Login</h1>
-        <p className="user-description subtitle">Sing in to your account</p>
-      </div>
-      <form>
-        <div className="input">
-          <i class="ri-user-line"></i>
-          <input type="text" placeholder="Username" required />
+      <div className="user-container">
+        <div className="user-content">
+          <h1 className="main-title user-title">Login</h1>
+          <p className="user-description subtitle">Sing in to your account</p>
         </div>
-        <div className="input">
-          <i class="ri-lock-line"></i>
-          <input type="password" placeholder="Password" required />
+        <form>
+          <div className="input">
+            <i class="ri-user-line"></i>
+            <input type="text" placeholder="Username" required />
+          </div>
+          <div className="input">
+            <i class="ri-lock-line"></i>
+            <input type="password" placeholder="Password" required />
+          </div>
+          <button type="submit" className="user-button btn btn-primary">
+            Login
+          </button>
+        </form>
+        <div className="user-footer">
+          <a href="">Forgot password?</a>
+          <p>
+            Don't have an account? <a href="/register" className="highlight">Register</a>
+          </p>
         </div>
-        <button type="submit" className="user-button">
-          Login
-        </button>
-      </form>
-      <div className="user-footer">
-        <a href="">Forgot password?</a>
-        <p>
-          Don't have an account? <a href="/signup">Sign up</a>
-        </p>
       </div>
     </div>
   );
