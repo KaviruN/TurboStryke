9d4594212fd83b0ed80a9613f5b322e1ea3a8ad2
KaviruN <kavirunethsara1@gmail.com>
2025-07-11 11:32:19 +0530

    feat: update Login page structure and styles for consistency

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,32 @@
 function Login() {
   document.title = "Login";
-  <div className="login">
-    <div className="login-content">
-      <h1 className="login-title">Login</h1>
-      <p className="login-description">Sing in to your account</p>
-    </div>
-    <form>
-      <div className="input">
-        <i class="ri-user-line"></i>
-        <input type="text" placeholder="Username" required />
+  return (
+    <div className="login">
+      <div className="login-content">
+        <h1 className="login-title">Login</h1>
+        <p className="login-description">Sing in to your account</p>
       </div>
-      <div className="input">
-        <i class="ri-lock-line"></i>
-        <input type="password" placeholder="Password" required />
+      <form>
+        <div className="input">
+          <i class="ri-user-line"></i>
+          <input type="text" placeholder="Username" required />
+        </div>
+        <div className="input">
+          <i class="ri-lock-line"></i>
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+      <div className="login-footer">
+        <a href="">Forgot password?</a>
+        <p>
+          Don't have an account? <a href="/signup">Sign up</a>
+        </p>
       </div>
-      <button type="submit" className="login-button">
-        Login
-      </button>
-    </form>
-    <div className="login-footer">
-      <a href="">Forgot password?</a>
-      <span>
-        Don't have an account? <a href="/signup">Sign up</a>
-      </span>
     </div>
-  </div>;
+  );
 }
 
 export default Login;
