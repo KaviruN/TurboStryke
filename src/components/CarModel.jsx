93c9d5a26a75202a9ef3b85fe32db168cfc56748
KaviruN <kavirunethsara1@gmail.com>
2025-07-13 11:10:41 +0530

    Remove Suspense wrapper from CarModels component and preload model

--- src/components/CarModel.jsx
+++ src/components/CarModel.jsx
@@ -1,7 +1,9 @@
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
-import { Suspense } from "react";
 import laferrariModel from "../assets/laferrari.glb";
+
+// Preload the model
+useGLTF.preload(laferrariModel);
 
 function Model(props) {
   const { scene } = useGLTF(laferrariModel);
@@ -16,24 +18,18 @@ function CarModels() {
       camera={{ fov: 40 }}
       className="car-models-canvas"
     >
-      <Suspense fallback={null}>
-        <PresentationControls
-          speed={1.5}
-          global
-          zoom={1}
-          polar={[-0.1, Math.PI / 4]}
-        >
-          <Stage environment={null} intensity={0.1} shadows={false}>
-            <ambientLight intensity={0.05} />
-            <directionalLight
-              position={[5, 5, 5]}
-              intensity={0.15}
-              castShadow
-            />
-            <Model scale={0.01} />
-          </Stage>
-        </PresentationControls>
-      </Suspense>
+      <PresentationControls
+        speed={1.5}
+        global
+        zoom={1}
+        polar={[-0.1, Math.PI / 4]}
+      >
+        <Stage environment={null} intensity={0.1} shadows={false}>
+          <ambientLight intensity={0.05} />
+          <directionalLight position={[5, 5, 5]} intensity={0.15} castShadow />
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
     </Canvas>
   );
 }
